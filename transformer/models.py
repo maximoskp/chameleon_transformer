e5062f8ba5b65992d198b5bfe641c1d858914520
maximoskp <maximoskalpap@gmail.com>
2024-01-29 22:21:32 +0200

    encoder decoder binary

--- transformer/models.py
+++ transformer/models.py
@@ -173,17 +173,16 @@ class ContinuousEncoder(nn.Module):
     # end init
 
     def generate_mask(self, src, pad_token_idx=0):
-        src_mask = (src != pad_token_idx).unsqueeze(1).unsqueeze(2)
+        src_mask = (torch.sum(src, dim=2) != 0).unsqueeze(1).unsqueeze(2)
         return src_mask
     # end generate_mask
 
     def forward(self, src, pad_token_idx=0):
-        # src_mask = self.generate_mask(src, pad_token_idx)
+        src_mask = self.generate_mask(src)
         src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
         enc_output = src_embedded
         for enc_layer in self.encoder_layers:
-            enc_output = enc_layer(enc_output, None)
-            # enc_output = enc_layer(enc_output, src_mask)
+            enc_output = enc_layer(enc_output, src_mask)
         return enc_output
     # end forward
 # end class ContinuousEncoder
@@ -197,9 +196,40 @@ class ContinuousEncoderOnlyWrapper(nn.Module):
         self.decoder = nn.Linear(n_dim, n_vocab)
     # end init
 
-    def forward(self, input_ids):
+    def forward(self, input_ids, pad_token_idx=0):
         output = self.encoder_model(input_ids)
         logits_vocab_size = self.decoder(output)
         return logits_vocab_size
     # end forward
 # end class ContinuousEncoderOnlyWrapper
+
+class ContinuousDecoderModel(nn.Module):
+    def __init__(self, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout):
+        super(ContinuousDecoderModel, self).__init__()
+        self.decoder_embedding = nn.Linear(tgt_vocab_size, d_model)
+        self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
+        self.decoder_layers = nn.ModuleList([DecoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
+        self.fc = nn.Linear(d_model, tgt_vocab_size)
+        self.dropout = nn.Dropout(dropout)
+    # end init
+
+    def generate_mask(self, src, tgt):
+        src_mask = (torch.sum(src, dim=2) != 0).unsqueeze(1).unsqueeze(2).to(dev)
+        tgt_mask = (torch.sum(tgt, dim=2) != 0).unsqueeze(1).unsqueeze(3).to(dev)
+        seq_length = tgt.size(1)
+        nopeak_mask = (1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=1)).bool().to(dev)
+        tgt_mask = tgt_mask & nopeak_mask
+        return src_mask, tgt_mask
+    # end generate_mask
+
+    def forward(self, enc_output, src, tgt, pad_token_idx=0):
+        src_mask, tgt_mask = self.generate_mask(src, tgt)
+        tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
+        dec_output = tgt_embedded
+        for dec_layer in self.decoder_layers:
+            dec_output = dec_layer(dec_output, enc_output, src_mask, tgt_mask)
+
+        output = self.fc(dec_output)
+        return output
+    # end forward
+# end class ContinuousDecoderModel
